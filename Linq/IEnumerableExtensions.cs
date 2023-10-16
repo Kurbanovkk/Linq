1687985003ce608c990aac099ce3e4dd7de5d2aa
Kurban <kurbanovkk96@mail.ru>
2023-10-16 08:48:59 +0300

    Добавьте файлы проекта.

---
+++ Linq/IEnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int percentage)
+        {
+            if (percentage < 1 || percentage > 100)
+            {
+                throw new ArgumentException("Число должно быть от 1 до 100.");
+            }
+
+            int count = (int)Math.Ceiling(source.Count() * percentage / 100.0);
+            return source.OrderByDescending(item => item).Take(count);
+        }
+
+        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> source, int percentage, Func<T, TKey> keySelector)
+        {
+            if (percentage < 1 || percentage > 100)
+            {
+                throw new ArgumentException("Число должно быть от 1 до 100.");
+            }
+
+            int count = (int)Math.Ceiling(source.Count() * percentage / 100.0);
+            return source.OrderByDescending(keySelector).Take(count);
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var topItems = list.Top(30);
+            Console.WriteLine(string.Join(", ", topItems));
+
+            var people = new List<Person>
+            {
+                new Person { Name = "Амир", Age = 25 },
+                new Person { Name = "Ахмад", Age = 30 },
+                new Person { Name = "Алихан", Age = 40 },
+                new Person { Name = "Абубакр", Age = 35 },
+            };
+
+            var topAgedPeople = people.Top(50, person => person.Age);
+            foreach (var person in topAgedPeople)
+            {
+                Console.WriteLine($"{person.Name} ({person.Age} лет)");
+            }
+        }
+    }
+}
